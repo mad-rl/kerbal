f9f077fac9989d59752a1ec5951d299783600cfc
Gabriel Munoz <gabriel.munoz@intelygenz.com>
2020-07-03 16:16:32 +0200

    monitor values and rewards on engine

--- src/engine.py
+++ src/engine.py
@@ -18,10 +18,12 @@ class Worker(object):
             observation_space=self.env.observation_space.shape[0])
 
     def work(self):
-        global global_rewards, global_episodes
+        global global_rewards, global_values, global_episodes
 
         max_episode_steps = 50
         obs = self.env.reset(self.conn)
+
+        rewards = []
 
         done = False
         episode_reward = 0
@@ -29,11 +31,13 @@ class Worker(object):
         episode_steps = 0
         while not done:
             for _ in range(max_episode_steps):
-                action = self.agent.get_action(obs)
+                action, value = self.agent.get_action(obs)
 
                 next_obs, reward, done, info = self.env.step(action)
                 episode_reward += reward
 
+                rewards.append(reward)
+                global_values.append(value.item())
                 self.agent.add_experience(obs, reward, action, next_obs, info)
 
                 obs = next_obs
@@ -44,20 +48,22 @@ class Worker(object):
                     global_rewards.append(episode_reward)
                     altitude = self.env.get_altitude()
 
-                    print(f"{self.name} - Episode: {global_episodes:4} "
-                        f"| Total Steps: {total_steps}"
-                        f"| Episode Steps: {episode_steps}"
-                        f"| Reward: {global_rewards[-1]:7.1f}" 
+                    print(f"{self.name} - Episode: {global_episodes} "
+                        f"| Total Steps: {total_steps} "
+                        f"| Episode Steps: {episode_steps} "
+                        f"| Reward: {global_rewards[-1]:7.1f} "
+                        f"| Mean episode reward: {np.mean(rewards)} "
+                        f"| Mean accumulated value: {np.mean(global_values)} "
                         f"| Altitude: {altitude:7.1f}")
 
                     global_episodes += 1
                     episode_reward = 0
                     episode_steps = 0
+                    rewards = []
                     obs = self.env.reset(self.conn)
                     done = False
                     break
 
-            print("Training agent...")
             self.agent.train()
 
 
@@ -69,6 +75,7 @@ if __name__ == "__main__":
     connections = [krpc.connect(**conns[0])]
     
     global_rewards = []
+    global_values = []
     global_episodes = 0
 
     print("Starting process")
